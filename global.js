9d524995524f4f4d2ccfb364ad45de0014b11225
JerpBob <jerupaulbalares@gmail.com>
2025-01-24 18:25:17 -0800

    Testing with github6

--- global.js
+++ global.js
@@ -18,9 +18,9 @@ const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
 let pages = [
     { url: 'portfolio/', title: 'Home' },
-    { url: '../projects/', title: 'Projects' },
-    { url: '../contact/', title: 'Contact' },
-    { url: '../resume/', title: 'Resume' },
+    { url: '../portfolio/projects/', title: 'Projects' },
+    { url: 'portfolio/contact/', title: 'Contact' },
+    { url: 'portfolio/resume/', title: 'Resume' },
     { url: 'https://github.com/Jerpbob', title: 'Github' }
 ];
 console.log(pages)
